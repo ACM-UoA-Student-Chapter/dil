d99a9668bd0c78fc1760613b963bf58f958aeebc
Spyros Trifonidis <spyrostrifon@protonmail.com>
2019-11-29 18:28:03 +0200

    TDD boilerplate

--- tests/lp_hash_table_test.cpp
+++ tests/lp_hash_table_test.cpp
@@ -1,7 +1,15 @@
-
+#include <assert.h>
 #include "lp_hash_table.h"
 #include "./lp_hash_table_test.h"
 
 int test_hash_table() {
+    lp_hash_table<int> table = lp_hash_table<int>();
+
+    int res = table.insert("key", 1);
+    assert(res == 0);
+
+    int val = table.find("key");
+    assert(val == 1);
+
     return 0;
 }
