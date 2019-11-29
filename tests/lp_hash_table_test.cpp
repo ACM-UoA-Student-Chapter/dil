9cc7c492204e9486c96e5b6d6982330fea39a378
Spyros Trifonidis <spyrostrifon@protonmail.com>
2019-11-29 22:04:45 +0200

    better tests

--- tests/lp_hash_table_test.cpp
+++ tests/lp_hash_table_test.cpp
@@ -1,15 +1,29 @@
+#include <iostream>
 #include <assert.h>
 #include "lp_hash_table.h"
 #include "./lp_hash_table_test.h"
 
-int test_hash_table() {
-    lp_hash_table<int> table = lp_hash_table<int>();
 
-    int res = table.insert("key", 1);
+typedef struct test_type {
+public:
+    int val;
+} test_type;
+
+int test_hash_table() {
+    std::cout << "Creating table of 1 element" << std::endl;
+    lp_hash_table<test_type> table = lp_hash_table<test_type>(1);
+
+    test_type t;
+    t.val = 1;
+    std::cout << "Inserting 1 element with value 1 into hash table" << std::endl;
+    int res = table.insert("key", t);
     assert(res == 0);
 
-    int val = table.find("key");
-    assert(val == 1);
+    test_type r;
+    std::cout << "Trying to find that element" << std::endl;
+    int ret = table.find("key", &r);
+    assert(ret == 0);
+    assert(r.val == 1);
 
     return 0;
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,11 @@
 #include "./lp_hash_table_test.h"
 #include <assert.h>
+#include <iostream>
 
 int main() {
     int res = test_hash_table();
     assert(res == 0);
+
+    std::cout << "\x1b[0;32m All tests passed! \x1b[0m" << std::endl;
+    return 0;
 }
