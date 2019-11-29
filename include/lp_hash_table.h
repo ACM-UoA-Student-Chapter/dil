bd5032565a03c2b20bc5930a6afd75a3dab920d4
Spyros Trifonidis <spyrostrifon@protonmail.com>
2019-11-29 18:24:53 +0200

    naive hash table constructor

--- include/lp_hash_table.h
+++ include/lp_hash_table.h
@@ -1,15 +1,33 @@
 #ifndef DIL_LP_HASH_TABLE_H
 #define DIL_LP_HASH_TABLE_H
 
+#include <stdlib.h>
+#include <assert.h>
+
 template <typename T>
 struct lp_hash_table {
 private:
     const char **pointers;
     T *data;
+    int buckets;
+
+    unsigned int hash(const char *key) {
+
+    };
 
 public:
 
-    lp_hash_table() {};
+    /*
+     * Constructor. Assumes that `elems > 0`
+     */
+    lp_hash_table(int elems) {
+        assert(elems > 0);
+
+        buckets = elems;
+        pointers = (const char **)malloc(buckets * sizeof(const char *));
+
+        data = (T *)malloc(buckets * sizeof(T));
+    };
 
     /*
      * @return - 0 means everything went ok, -1 means there was an error
