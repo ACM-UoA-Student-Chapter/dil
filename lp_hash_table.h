90224d654a7e27837f88a7d6f016c6f7b27570be
Spyros Trifonidis <spyrostrifon@protonmail.com>
2019-11-29 17:04:53 +0200

    Generic map key'd by interned strings

---
+++ lp_hash_table.h
@@ -0,0 +1,134 @@
+/*
+* Copyright: Copyright Spyros Trifonidis (@spytfyre) 2019.
+* License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+* Authors:   Spyros Trifonidis (@spytfyre)
+*/
+#ifndef DIL_LP_HASH_TABLE_H
+#define DIL_LP_HASH_TABLE_H
+
+#include <assert.h>
+#include <functional>
+#include <stdint.h>
+#include <stdlib.h>
+#include <type_traits>
+
+/*
+ * LpHashTable is a map of key-value pairs where keys are `const char *`
+ * and values are of any type. The supported operations are insertion and search.
+ *
+ * NOTE: The number of the emelements to be stored is constant and provided in
+ * the map's construction
+ *
+ * NOTE: The contents of the key K do not matter when inserting and searching.
+ * That is because the map uses the pointer value to insert and search.
+ * IE the keys are handled like integer keys.
+ *
+ * NOTE: The type T of the values provided must be of object pointer.
+ *
+ * Performance:
+ * Insertion: O(1) amortized.
+ * Search: O(1) amortized.
+ */
+template <typename T> struct LpHashTable {
+/*
+ * The map is implemented as a linear probing hash table. There are 2 parallel
+ * arrays, one for keys and one for values. The keys are hashed by their pointer
+ * and are inserted into the table at the first empty spot. If the hashed spot
+ * is empty, we insert the key there and the value in its parallel spot.
+ * Otherwise, we search linearly for the next empty key spot (i.e. linear probe)
+ * and do the same thing. Find is implemented in a similar fashion. Delete is
+ * not implemented as we have no need for it. Notes:
+ *  - The load factor is a constant 70% (the number of elements in the table is
+ * constant).
+ *  - The values must be pointers. This constraint is imposed in order to save
+ * space, as some of it is wasted to keep a load factor of 70%.
+ */
+  static const bool is_pointer = std::is_pointer<T>::value;
+  static const bool is_function_pointer =
+      is_pointer
+          ? std::is_function<typename std::remove_pointer<T>::type>::value
+          : false;
+  static_assert(is_pointer && !is_function_pointer,
+                "T must be an object pointer but not a function pointer");
+
+private:
+  const char **keys;
+  T *values;
+  ssize_t nbuckets;
+
+  ssize_t hash(const char *key) {
+    // TODO: implement a better hashing method
+    return (ssize_t)((intptr_t)key % this->nbuckets);
+  }
+
+  ssize_t find_index(const char *key) {
+
+    ssize_t index = this->hash(key);
+    ssize_t i = index;
+
+    while (this->keys[i] != nullptr) {
+      if (this->keys[i] == key) {
+        return i;
+      }
+
+      i++;
+      if (i == this->nbuckets)
+        i = 0; // Wrap around at the start
+      if (i == index)
+        return -1; // We have traversed the whole table
+    }
+
+    return i; // We found an empty spot.
+  }
+
+public:
+  /*
+   * Constructor. Assumes that `elems > 0`
+   */
+  LpHashTable(ssize_t elems) {
+    assert(elems > 0);
+
+    this->nbuckets = elems * 1.3;
+
+    // TODO: use our own allocator when it is ready.
+    keys = (const char **)calloc(this->nbuckets, sizeof(const char *));
+    values = (T *)calloc(this->nbuckets, sizeof(T));
+
+    assert(keys != NULL);
+    assert(values != NULL);
+  };
+
+  /*
+   * @return. nothing. we are sure that we can insert
+   */
+  void insert(const char *key, T value) {
+    assert(key != NULL);
+    assert(value != NULL);
+
+    ssize_t i = find_index(key);
+    assert(i != -1);
+    assert(this->keys[i] != key);
+    this->keys[i] = key;
+    this->values[i] = value;
+  };
+
+  /*
+   * @return - Copy of the value under key or NULL if it does not exist
+   */
+  T find(const char *key) {
+    assert(key != NULL);
+
+    ssize_t i = find_index(key);
+    if (i == -1) {
+      return NULL;
+    }
+    if (this->keys[i] != nullptr) {
+      assert(this->values[i] != nullptr);
+      return this->values[i];
+    } else {
+      return NULL;
+    }
+  };
+};
+
+#endif // DIL_LP_HASH_TABLE_H
---
+++ tests/lp_hash_table_test.cpp
@@ -0,0 +1,99 @@
+/*
+* Copyright: Copyright Spyros Trifonidis (@spytfyre) 2019.
+* License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+* Authors:   Spyros Trifonidis (@spytfyre)
+*/
+#include "../lp_hash_table.h"
+#include <assert.h>
+#include <iostream>
+#include <cstring>
+
+class test_type {
+public:
+  int val;
+
+  test_type(int v) { this->val = v; }
+};
+
+bool test_hash_table() {
+  LpHashTable<test_type *> table = LpHashTable<test_type *>(1);
+
+  const char *k = "key";
+  test_type *t = new test_type(1);
+  table.insert(k, t);
+
+  test_type *r;
+  r = table.find(k);
+  assert(r->val == 1);
+
+  return true;
+}
+
+bool test_many_key_insertion() {
+
+  LpHashTable<test_type *> table = LpHashTable<test_type *>(5);
+
+  test_type *t1 = new test_type(1);
+  test_type *t2 = new test_type(2);
+  test_type *t3 = new test_type(3);
+  test_type *t4 = new test_type(4);
+  test_type *t5 = new test_type(5);
+
+  const char *k1 = "key1";
+  const char *k2 = "key2";
+  const char *k3 = "key3";
+  const char *k4 = "key4";
+  const char *k5 = "key5";
+  table.insert(k1, t1);
+  table.insert(k2, t2);
+  table.insert(k3, t3);
+  table.insert(k4, t4);
+  table.insert(k5, t5);
+
+  test_type *r;
+
+  r = table.find(k1);
+  assert(r->val = 1);
+
+  r = table.find(k2);
+  assert(r->val = 2);
+
+  r = table.find(k3);
+  assert(r->val = 3);
+
+  r = table.find(k4);
+  assert(r->val = 4);
+
+  r = table.find(k5);
+  assert(r->val = 5);
+
+  return true;
+}
+
+bool test_memory_location_functionality() {
+  LpHashTable<test_type *> table = LpHashTable<test_type *>(3);
+
+  test_type *t1 = new test_type(1);
+
+  // Dynamically allocate k2 so k1 and k2 have different addresses in memory
+  const char *k1 = "key1";
+  char *k2 = strdup(k1);
+  assert((intptr_t)k1 != (intptr_t)k2);
+
+  const char *not_exist = "not_exist";
+
+  table.insert(k1, t1);
+
+  test_type *r;
+
+  // test that we get NULL for keys that don't exist
+  r = table.find(not_exist);
+  assert(r == NULL);
+
+  // test that two keys with identical characters
+  // having different memory addresses fail find
+  r = table.find(k2);
+  assert(r == NULL);
+
+  return true;
+}
