7cfcea537e9dd4e074741c042aea22d22d958973
Spyros Trifonidis <spyrostrifon@protonmail.com>
2019-11-30 13:01:47 +0200

    abstract the index finding method

--- include/lp_hash_table.h
+++ include/lp_hash_table.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <assert.h>
 
+
 template <typename T>
 struct lp_hash_table {
 private:
@@ -15,6 +16,26 @@ private:
     unsigned int hash(const char *key) {
         // TODO: implement a better hashing method
         return ((uintptr_t)key % this->nbuckets);
+    }
+
+    int find_index(const char *key) {
+
+        int index = this->hash(key);
+        int i = index;
+
+        while(this->keys[i] != nullptr) {
+            if (this->keys[i] == key) {
+                return i;
+            }
+
+            i++;
+            if (i == this->nbuckets)
+                i = 0; // Wrap around at the start
+            if (i == index)
+                return -1; // We have traversed the whole table
+        }
+
+        return i; // We found an empty spot.
     }
 
 public:
@@ -38,55 +59,32 @@ public:
      */
     bool insert(const char *key, T value) {
 
-        int index = this->hash(key);
-        int i = index;
-
-        while(this->keys[i] != nullptr) {
-            if (this->keys[i] == key) {
-                return false;
-            }
-
-            i++;
-            if (i == this->nbuckets)
-                i = 0; // Wrap around at the start
-            if (i == index)
-                return false; // We have traversed the whole table and found no spot to insert in
+        int i = find_index(key);
+        if(i != -1 && this->keys[i] != key) {
+            this->keys[i] = key;
+            this->values[i] = value;
+            return true;
         }
-
-        this->keys[i] = key;
-        this->values[i] = value;
-        return true;
+        return false;
     };
 
     /*
      * @return - Copy of the value under key or NULL if it does not exist
      */
     T* find(const char *key) {
-        int index = this->hash(key);
 
-        int i = index;
-
-        while(this->keys[i] != nullptr) {
-            if (this->keys[i] == key) {
+        int i = find_index(key);
+        if(i != -1) {
+            if (this->keys[i] != nullptr) {
                 assert(this->keys[i] != nullptr);
                 return &this->values[i];
             } else if (this->keys[i] == nullptr) {
                 return NULL;
             }
-
-            i++;
-            if (i == this->nbuckets)
-                i = 0; // Wrap around at the start
-            if (i == index)
-                return NULL; // We have traversed the whole table and didn't find the element
         }
         return NULL;
-    };
 
-    /*
-     * We apparently don't need this
-     * int remove(const char *key);
-     */
+    };
 };
 
 #endif //DIL_LP_HASH_TABLE_H
