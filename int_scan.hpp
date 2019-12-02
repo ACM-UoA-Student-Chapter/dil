7eaa2ec7e317091e32be7140878e0ab509c14ceb
nsoul <nikossl97@gmail.com>
2019-12-02 21:43:47 +0200

    Integer scanning

---
+++ int_scan.hpp
@@ -0,0 +1,12 @@
+#ifndef __INT_SCAN_HPP
+#define __INT_SCAN_HPP
+
+#define INT_SCANNED 0
+#define OVERFLOW_ERROR -1
+#define OUT_OF_BOUNDS_ERROR -2
+#define EMPTY_NUM_EXPR_ERROR -3
+
+int read_int(const char *input, int *val);
+void init_char_to_digit();
+
+#endif
