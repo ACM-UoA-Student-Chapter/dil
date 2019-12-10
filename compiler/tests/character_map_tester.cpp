18df414715da4cea72ff6ad1268aae1b79dc0625
TheodoreAlenas <58252759+TheodoreAlenas@users.noreply.github.com>
2019-12-11 00:23:02 +0200

    The character map, as an array
    The character map that stores the properties of the ASCII characters, such as whether they represent digits,
    or whether they can appear in an identifier.

---
+++ compiler/tests/character_map_tester.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright:  Copyright Dimakopoulos Theodoros 2019
+ * License:    $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Authors:    Dimakopoulos Theodoros
+ */
+#include "character_map.h"
+#include "character_map_tester.h"
+
+#include <cctype>
+using std::tolower;
+
+#include <assert.h>
+ 
+
+
+void ch_map_tester() {
+
+  for (char c = 0; c < 127; ++c) {
+    char lc = tolower(c);
+
+    // "'0' <= c <= '9'" must be as true as "c being decimal"
+    assert(('0' <= lc && lc <= '9') == (bool)(ch_map[c] & CM_DECIMAL));
+    assert(('0' <= lc && lc <= '7') == (bool)(ch_map[c] & CM_OCTAL));
+    assert(('a' <= lc && lc <= 'f' || '0' <= lc && lc <= '9') ==
+           (bool)(ch_map[c] & CM_HEXADECIMAL));
+
+    bool is_ident = false; // is c an identifier character?
+    if ('0' <= lc && lc <= '9')
+      is_ident = true;
+    if ('a' <= lc && lc <= 'z')
+      is_ident = true;
+    if (lc == '_')
+      is_ident = true;
+
+    assert(is_ident == (bool)(ch_map[c] & CM_IDENTIFIER));
+
+    // blank characters
+    assert((c == ' ' || c == '\t' || c == '\n') ==
+           (bool)(ch_map[c] & CM_BLANK));
+  }
+}
