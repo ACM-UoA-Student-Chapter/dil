efbb8a4d4a18afc754cddd7a1bf7c91480412462
J1635 <orestisg@me.com>
2019-12-11 17:52:49 +0200

    editted null case

--- compiler/test_intern_string.cpp
+++ compiler/test_intern_string.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[]) {
   const char *input_1 = "test";
   const char *input_2 = "test 2";
   const char *input_3 = "";
-  const char *input_4;
+  const char *input_4 = NULL;
 
   const char *address_1 = intern_string(input_1);
   const char *address_2 = intern_string(input_2);
