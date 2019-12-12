ae4bbfccb86fe50d01289e36c17c977bb54c5007
nsoul <nikossl97@gmail.com>
2019-12-12 20:52:08 +0200

    print token given the enumeration

---
+++ compiler/token_name.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright: Copyright nsoul97 (Soulounias Nikolaos) 2019.
+ * License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Authors:   nsoul97 (Soulounias Nikolaos)
+ */
+
+#ifndef __TOKEN_NAME_H
+#define __TOKEN_NAME_H
+
+#include "tokens.h"
+
+const char *token_name(TOK kind);
+
+#endif
