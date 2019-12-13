d68093d5c6517b6331971ae6c2fdf5f8059fa948
ssiatras <ssiatras@outlook.com>
2019-12-13 11:26:12 +0200

    Use stringstream method

--- compiler/read_file.cpp
+++ compiler/read_file.cpp
@@ -4,15 +4,13 @@
 * Authors:   ssiatras (Stathis Siatras)
 */
 #include "read_file.h"
-#include <cstdio>
-#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 /*
  * This function reads the content of a text file as a whole into memory as a
  * NULL-terminated, contiguous array of characters.
- *
- * It only supports ASCII files with one-byte endlines;
- * (e.g. UNIX format endlines).
  *
  * Params:
  *    filepath = The path of the file to be read.
@@ -24,40 +22,20 @@ const char *read_file_into_memory(const char *filepath) {
     return NULL;
   }
 
-  FILE *finput = fopen(filepath, "r");
-  if (finput == NULL) {
-    return NULL;
+  std::ifstream finput(filename, std::ios::in | std::ios::binary);
+  if (!finput) {
+  	return NULL;
   }
 
-  if (fseek(finput, 0, SEEK_END) != 0) {
-    fclose(finput);
-    return NULL;
-  }
+  std::ostringstream contents;
+  contents << finput.rdbuf();
+  finput.close();
 
-  long int fsize = ftell(finput);
-  if (fsize == -1) {
-    fclose(finput);
-    return NULL;
-  }
+  std::string str = contents.str();
+  long int fsize = str.size();
 
-  if (fseek(finput, 0, SEEK_SET) != 0) {
-    fclose(finput);
-    return NULL;
-  }
+  char *cstr = new char[fsize + 1];
+  strcpy(cstr, str.c_str());
 
-  char *string = new char[fsize + 1];
-  if (string == NULL) {
-    fclose(finput);
-    return NULL;
-  }
-  if (fread(string, 1, fsize, finput) != fsize) {
-    return NULL;
-  }
-  string[fsize] = '\0';
-
-  if (fclose(finput) == EOF) {
-    return NULL;
-  }
-
-  return string;
+  return cstr;
 }
