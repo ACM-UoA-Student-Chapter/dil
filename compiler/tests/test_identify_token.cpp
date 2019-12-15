7040a31e7839973f318b63c51b4a230cb7e77400
nsoul <nikossl97@gmail.com>
2019-12-15 12:00:21 +0200

    swapped function with macro, deleted print msg

--- compiler/tests/test_identify_token.cpp
+++ compiler/tests/test_identify_token.cpp
@@ -5,7 +5,6 @@
  */
 
 #include "../identify_token.h"
-#include "../token_name.h"
 #include "../tokens.h"
 #include <cassert>
 #include <cstdio>
@@ -13,24 +12,15 @@
 extern Token token;
 
 /*
-If the global variable token has its kind member set to the expected_enum, the
-function prints a success message. Otherwise, the program execution stops due to
+If the global variable token doesn't have its kind member set to the
+expected_enum, the macto TEST function stops the program execution stops due to
 the assertion.
 */
-static void test(const char *input, TOK expected_enum) {
-  identify_token(input);
-  assert(token.kind == expected_enum);
-  if (expected_enum != TOK::UNDEFINED)
-    printf("SUCCESS: function identify_token(\"%s\") sets token's kind member "
-           "to %s token.\n",
-           input, token_name(expected_enum));
-  else
-    printf(
-        "SUCCESS: function identify_token(\"%s\") sets token's kind member to "
-        "UNDEFINED token.\n\t \"%s\" starts with an integer literal, an "
-        "identifier, a blank character or a non-DIL string.\n",
-        input, input);
-}
+#define TEST(input_string, expected_enum)                                      \
+  {                                                                            \
+    identify_token(input_string);                                              \
+    assert(token.kind == expected_enum);                                       \
+  }
 
 /*
 This function is used for testing various input strings as arguments of the
@@ -40,97 +30,97 @@ int main(void) {
   init_isIdChar();
   init_isBlankChar();
 
-  test("", TOK::EOI);
-  test("int i;", TOK::INT);
-  test("bool var;", TOK::BOOL);
-  test("struct Student;", TOK::STRUCT);
-  test("new int[5];", TOK::NEW);
-  test("func swap(arr : int[], i, j : int)", TOK::FUNC);
-  test("if (x>10)", TOK::IF);
-  test("else{", TOK::ELSE);
-  test("return x;", TOK::RETURN);
-  test("while (i<10)", TOK::WHILE);
-  test("false || true", TOK::FALSE);
-  test("true||false", TOK::TRUE);
+  TEST("", TOK::EOI);
+  TEST("int i;", TOK::INT);
+  TEST("bool var;", TOK::BOOL);
+  TEST("struct Student;", TOK::STRUCT);
+  TEST("new int[5];", TOK::NEW);
+  TEST("func swap(arr : int[], i, j : int)", TOK::FUNC);
+  TEST("if (x>10)", TOK::IF);
+  TEST("else{", TOK::ELSE);
+  TEST("return x;", TOK::RETURN);
+  TEST("while (i<10)", TOK::WHILE);
+  TEST("false || true", TOK::FALSE);
+  TEST("true||false", TOK::TRUE);
 
-  test("01210 + 100", TOK::UNDEFINED);
-  test("var_name", TOK::UNDEFINED);
-  test("intvar", TOK::UNDEFINED);
-  test("function", TOK::UNDEFINED);
+  TEST("01210 + 100", TOK::UNDEFINED);
+  TEST("var_name", TOK::UNDEFINED);
+  TEST("intvar", TOK::UNDEFINED);
+  TEST("function", TOK::UNDEFINED);
 
-  test(" int x;", TOK::INT);
-  test("\t+10;", TOK::PLUS);
-  test("\nbool b;", TOK::BOOL);
-  test("\t\n\t\t   while (true) ", TOK::WHILE);
+  TEST(" int x;", TOK::INT);
+  TEST("\t+10;", TOK::PLUS);
+  TEST("\nbool b;", TOK::BOOL);
+  TEST("\t\n\t\t   while (true) ", TOK::WHILE);
 
-  test("== 10", TOK::EQ);
-  test("==10", TOK::EQ);
+  TEST("== 10", TOK::EQ);
+  TEST("==10", TOK::EQ);
 
-  test("!= 07", TOK::NEQ);
-  test("!= 07", TOK::NEQ);
+  TEST("!= 07", TOK::NEQ);
+  TEST("!= 07", TOK::NEQ);
 
-  test(">_var", TOK::GT);
-  test("> -12", TOK::GT);
+  TEST(">_var", TOK::GT);
+  TEST("> -12", TOK::GT);
 
-  test(">=_var", TOK::GEQ);
-  test(">=-12", TOK::GEQ);
+  TEST(">=_var", TOK::GEQ);
+  TEST(">=-12", TOK::GEQ);
 
-  test("<_var", TOK::LT);
-  test("< -12", TOK::LT);
+  TEST("<_var", TOK::LT);
+  TEST("< -12", TOK::LT);
 
-  test("<=_var", TOK::LEQ);
-  test("<= -12", TOK::LEQ);
+  TEST("<=_var", TOK::LEQ);
+  TEST("<= -12", TOK::LEQ);
 
-  test("! true", TOK::NOT);
-  test("!boolvar", TOK::NOT);
+  TEST("! true", TOK::NOT);
+  TEST("!boolvar", TOK::NOT);
 
-  test("+ 120", TOK::PLUS);
-  test("+120", TOK::PLUS);
+  TEST("+ 120", TOK::PLUS);
+  TEST("+120", TOK::PLUS);
 
-  test("- 0230", TOK::MINUS);
-  test("-0230", TOK::MINUS);
+  TEST("- 0230", TOK::MINUS);
+  TEST("-0230", TOK::MINUS);
 
-  test("* 0X1021", TOK::MUL);
-  test("*0X1021", TOK::MUL);
+  TEST("* 0X1021", TOK::MUL);
+  TEST("*0X1021", TOK::MUL);
 
-  test("/ 0xff10", TOK::DIV);
-  test("/0xff10", TOK::DIV);
+  TEST("/ 0xff10", TOK::DIV);
+  TEST("/0xff10", TOK::DIV);
 
-  test("% 10", TOK::MOD);
-  test("%10", TOK::MOD);
+  TEST("% 10", TOK::MOD);
+  TEST("%10", TOK::MOD);
 
-  test("&& x==10", TOK::AND_AND);
-  test("&&x==10", TOK::AND_AND);
-  test("& x==10", TOK::UNDEFINED);
-  test("&x==10", TOK::UNDEFINED);
+  TEST("&& x==10", TOK::AND_AND);
+  TEST("&&x==10", TOK::AND_AND);
+  TEST("& x==10", TOK::UNDEFINED);
+  TEST("&x==10", TOK::UNDEFINED);
 
-  test("|| x==10", TOK::OR_OR);
-  test("||x==10", TOK::OR_OR);
-  test("| x==10", TOK::UNDEFINED);
-  test("|x==10", TOK::UNDEFINED);
+  TEST("|| x==10", TOK::OR_OR);
+  TEST("||x==10", TOK::OR_OR);
+  TEST("| x==10", TOK::UNDEFINED);
+  TEST("|x==10", TOK::UNDEFINED);
 
-  test(":int", TOK::COLON);
-  test(": bool", TOK::COLON);
+  TEST(":int", TOK::COLON);
+  TEST(": bool", TOK::COLON);
 
-  test(", x , y;", TOK::COMMA);
-  test(",x,y;", TOK::COMMA);
+  TEST(", x , y;", TOK::COMMA);
+  TEST(",x,y;", TOK::COMMA);
 
-  test("[5]", TOK::LBRACKET);
-  test("[ 5 ]", TOK::LBRACKET);
-  test("] ", TOK::RBRACKET);
-  test("]", TOK::RBRACKET);
+  TEST("[5]", TOK::LBRACKET);
+  TEST("[ 5 ]", TOK::LBRACKET);
+  TEST("] ", TOK::RBRACKET);
+  TEST("]", TOK::RBRACKET);
 
-  test("(5)", TOK::LPAR);
-  test("( 5 )", TOK::LPAR);
-  test(") ", TOK::RPAR);
-  test(")", TOK::RPAR);
+  TEST("(5)", TOK::LPAR);
+  TEST("( 5 )", TOK::LPAR);
+  TEST(") ", TOK::RPAR);
+  TEST(")", TOK::RPAR);
 
-  test("{\treturn 5; }", TOK::LBRACE);
-  test("{return 5;}", TOK::LBRACE);
-  test("} ", TOK::RBRACE);
-  test("}", TOK::RBRACE);
+  TEST("{\treturn 5; }", TOK::LBRACE);
+  TEST("{return 5;}", TOK::LBRACE);
+  TEST("} ", TOK::RBRACE);
+  TEST("}", TOK::RBRACE);
 
-  test(".kind", TOK::DOT);
-  test(";", TOK::SEMICOLON);
+  TEST(".kind", TOK::DOT);
+  TEST(";", TOK::SEMICOLON);
   return 0;
 }
